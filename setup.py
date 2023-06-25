267bc67daf1a8c7cc50e399a14ce62f4bf4e74a7
youssefboumait <36265645+youssefboumait@users.noreply.github.com>
2023-06-25 21:55:46 +0100

    updated Readme for the new code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DevNet',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='bouma',
+    author_email='',
+    description=''
+)
